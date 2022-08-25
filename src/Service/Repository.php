3b5dd15cb1446a77a3437391a005c529741270b5
Pavel Chaplygin <mail@pchapl.dev>
2022-08-25 21:53:51 +0300

    Fixed with lazy by symfony/proxy-manager-bridge

--- src/Service/Repository.php
+++ src/Service/Repository.php
@@ -4,10 +4,9 @@ declare(strict_types=1);
 
 namespace App\Service;
 
-final class Repository
+class Repository
 {
     public function __construct(private readonly ComplicatedLogger $logger)
     {
-
     }
 }
